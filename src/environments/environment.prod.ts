f8996ff473c318020ab10745ed74ef0ddf245a18
Sven Schürmann <schuermann.sven@kiongroup.com>
2019-02-07 10:54:48 +0100

    NGX-Translate module configuration changed.
     - Loader factory changed.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,15 @@
 export const environment = {
   production: true,
   disableAuthentication: false,
+  config: {
+    url: 'https://yacovi-config-service.azurewebsites.net/api/GetYaCoViConfig'
+  },
   azure: {
     adal: {
       tenant: '13c728e0-bb0c-4cf7-8e10-5b327279d6d9',
-      clientId: '9046ce57-3082-4d8b-bec4-3b76ac73f4bc'
-    },
-    vault: {
-      url: 'https://yacovi-vault.vault.azure.net/secrets',
-      apiVersion: '7.0'
+      clientId: '9046ce57-3082-4d8b-bec4-3b76ac73f4bc',
+      redirectUri: 'http://localhost:4200/',
+      endpoints: ["https://vault.azure.net"]
     }
   }
 };
