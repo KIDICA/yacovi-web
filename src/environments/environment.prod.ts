018f3b76e7e184ebd8ab99ae6b10e1f442d94aab
Sven Schürmann <schuermann.sven@kiongroup.com>
2019-02-07 11:41:07 +0100

    Production Configuration changed.
     - Azure ADAL redirect URI updated.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
     adal: {
       tenant: '13c728e0-bb0c-4cf7-8e10-5b327279d6d9',
       clientId: '9046ce57-3082-4d8b-bec4-3b76ac73f4bc',
-      redirectUri: 'http://localhost:4200/',
+      redirectUri: 'https://kidica.github.io/yacovi-web/',
       endpoints: ["https://vault.azure.net"]
     }
   }
